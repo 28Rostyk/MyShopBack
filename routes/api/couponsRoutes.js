dd0d8ba0ceddb047b9975326a80125d01c5516da
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-06-02 11:11:44 +0300

    Update couponsRoutes.js

--- routes/api/couponsRoutes.js
+++ routes/api/couponsRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const couponsRouter = express.Router();
 
 const controllers = require("../../controllers");
-const { authenticate } = require("../../middlewares");
-const ctrlWrapper = require("../../helpers/ctrlWrapper");
+
+const ctrlWrapper = require("../../Helpers/ctrlWrapper");
 
 couponsRouter.get("/", ctrlWrapper(controllers.getAllCoupons));
 
